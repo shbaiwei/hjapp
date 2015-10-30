4bb6f3941ddb9ba8be04978c6e411a4e9abe5385
bruce <Bruce@brucedeimac.local>
2015-10-30 10:26:49 +0800

    Initial Commit

---
+++ HJApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HJApp
+//
+//  Created by Bruce He on 15/10/30.
+//  Copyright © 2015年 shanghai baiwei network technology. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
